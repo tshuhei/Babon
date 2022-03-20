d571d7187d7a26685f43b50404ca630221672a3a
tshuhei <tsushuhei@gmail.com>
2022-03-20 10:36:23 +0900

    Initial Commit

---
+++ Babon/BabonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabonApp.swift
+//  Babon
+//
+//  Created by 釣谷周平 on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct BabonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
