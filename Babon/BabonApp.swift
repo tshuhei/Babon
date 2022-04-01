cdb81a528b14957c1ec716b493549f570d00bddd
tshuhei <tsushuhei@gmail.com>
2022-04-01 23:55:50 +0900

    Added app icons and installed firebase functions

--- Babon/BabonApp.swift
+++ Babon/BabonApp.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct BabonApp: App {
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
                 StartMenuView()
             }
+        }
+    }
+    
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            FirebaseApp.configure()
+            
+            return true
         }
     }
 }
--- Babon/MultiPlayView.swift
+++ Babon/MultiPlayView.swift
@@ -6,10 +6,63 @@
 //
 
 import SwiftUI
+import FirebaseFunctions
 
 struct MultiPlayView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var roomID = ""
+    @State private var labelStr: String = "default"
+    let functions = Functions.functions()
+    
+    init(){
+        functions.useEmulator(withHost: "localhost", port: 5001)
+    }
+    
     var body: some View {
-        Text("Coming soon!")
+        ZStack {
+            VStack{
+                Spacer()
+                TextField("Input roomID", text: $roomID)
+                                .padding()
+                                .multilineTextAlignment(.center)
+                Spacer()
+                    .frame(height: 50)
+                Button(action: {
+                    print(roomID)
+                    functions.httpsCallable("helloWorld").call(["text": roomID]){ result, error in
+                        if let error = error{
+                            debugPrint(error.localizedDescription)
+                        }else{
+                            labelStr = result!.data as! String
+                        }
+                    }
+                }, label: {
+                    Text("Connect")
+                })
+                Spacer()
+                Text(labelStr)
+
+            }
+            .navigationBarHidden(true)
+            
+            VStack {
+                HStack {
+                    Button(
+                        action: {
+                            dismiss()
+                        }, label: {
+                            Image(systemName: "arrow.backward")
+                        }
+                    )
+                    .tint(.black)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding(.leading)
+            
+        }
+       
     }
 }
 
